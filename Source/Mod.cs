b6527ab6de545fbed17122e9767e599ba61bb23a
ZzZombo <ZzZombo@users.noreply.github.com>
2021-09-07 00:12:44 +0800

    -Refactored mod settings stub code. -Corrected multi-version support for RW v1.0. -Changed project to refer to the last supported RimWorld version when targeting `v1.x`. -Improved the organization of the project file. -Fixed wrong conditional defines for the debug configuration.

--- Source/Mod.cs
+++ Source/Mod.cs
@@ -4,7 +4,6 @@ internal static class MOD
 {
 	public const string ID = "*FIXME*";
 	public const string NAME = "*FIXME*";
-	public const bool SHOW_SETTINGS = false;
 }
 
 namespace ZzZomboRW
@@ -12,32 +11,29 @@ namespace ZzZomboRW
 	[HotSwappable]
 	internal class Mod: Verse.Mod
 	{
-		private ModSettings settings;
 		public static string ModID => $"{typeof(Mod).Namespace}.{MOD.ID}";
+#if MOD_SHOW_SETTINGS
+		private readonly ModSettings settings;
+#endif
 		public Mod(ModContentPack content) : base(content)
 		{
+#if MOD_SHOW_SETTINGS
 			this.settings = this.GetSettings<ModSettings>();
+#endif
 		}
-		/* public override void DoSettingsWindowContents(Rect rect)
+#if MOD_SHOW_SETTINGS
+		public override void DoSettingsWindowContents(UnityEngine.Rect rect)
 		{
 			var listingStandard = new Listing_Standard();
 			listingStandard.Begin(rect);
+			// TODO: add code.
 			listingStandard.End();
 			base.DoSettingsWindowContents(rect);
-		} */
+		}
 		public override string SettingsCategory()
 		{
-			if(!MOD.SHOW_SETTINGS)
-			{
-				return null;
-			}
-#pragma warning disable CS0162 // Unreachable code detected.
-			if(ModID.TryTranslate(out var r))
-			{
-				return r;
-			}
-			return MOD.NAME;
-#pragma warning restore CS0162 // Unreachable code detected.
+			return ModID.TryTranslate(out var r) ? (string)r : MOD.NAME;
 		}
+#endif
 	}
 }
